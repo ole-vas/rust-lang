80279577af9f0b1f38ad336449d0ddcde61cabe7
Oleksandr Vasylyuk <241250135+oleksandr-vasylyuk-next@users.noreply.github.com>
2025-11-30 21:46:45 +0200

    Commit changes

---
+++ restaurant/src/lib.rs
@@ -0,0 +1,37 @@
+fn deliver_order() {}
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from("toast"),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
+}
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+}
